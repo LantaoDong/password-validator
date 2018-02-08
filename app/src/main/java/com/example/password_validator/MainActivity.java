9f9e668aff2aa25124cd4f7387a9563c2bbae287
lantaodong <championruss@163.com>
2018-02-08 17:12:15 -0400

    added some comments

--- app/src/main/java/com/example/password_validator/MainActivity.java
+++ app/src/main/java/com/example/password_validator/MainActivity.java
@@ -10,12 +10,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    //rule 1: it is not "password"
     public static boolean notEqualPassword(String password){
         boolean NotEqualPassword = false;
         if(!password.toLowerCase().equals("password"))
             NotEqualPassword = true;
         return NotEqualPassword;
-    }
+    }//rule 2: at least 8 characters
     public static boolean atLeastEight(String password){
         boolean atLeastEight = false;
         if(password.length()>=8)
