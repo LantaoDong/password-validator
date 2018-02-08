d346ccf2fec6aac574f507d81e3bb8c91ee8c7ce
lantaodong <championruss@163.com>
2018-02-08 16:07:13 -0400

    a2-stage1 done!

--- app/src/main/java/com/example/password_validator/MainActivity.java
+++ app/src/main/java/com/example/password_validator/MainActivity.java
@@ -10,4 +10,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public static boolean notEqualPassword(String password){
+        boolean NotEqualPassword = false;
+        if(!password.toLowerCase().equals("password"))
+            NotEqualPassword = true;
+        return NotEqualPassword;
+    }
+    public static boolean atLeastEight(String password){
+        boolean atLeastEight = false;
+        if(password.length()>=8)
+            atLeastEight = true;
+        return atLeastEight;
+
+    }
 }
--- app/src/test/java/com/example/password_validator/ExampleUnitTest.java
+++ app/src/test/java/com/example/password_validator/ExampleUnitTest.java
@@ -1,6 +1,8 @@
 package com.example.password_validator;
 
 import org.junit.Test;
+import static com.example.password_validator.MainActivity.notEqualPassword;
+import static com.example.password_validator.MainActivity.atLeastEight;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +15,17 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void not_Password() throws Exception {
+        String pwd = "HelloWorld";
+        boolean notEqualPassword = notEqualPassword(pwd);
+        assertTrue(notEqualPassword);
+    }
+    @Test
+    public void AtLeastEight() throws Exception{
+        String pwd = "HelloWorld";
+        boolean atLeastEight = atLeastEight(pwd);
+        assertTrue(atLeastEight);
     }
 }
