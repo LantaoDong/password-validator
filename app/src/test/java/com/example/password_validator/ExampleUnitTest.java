9d22f68593f068b25bab3499aedd57c0b65a6b70
lantaodong <championruss@163.com>
2018-02-08 17:16:51 -0400

    a2-stage2

--- app/src/test/java/com/example/password_validator/ExampleUnitTest.java
+++ app/src/test/java/com/example/password_validator/ExampleUnitTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.*;
 import static com.example.password_validator.MainActivity.atLeastOneDigit;
 import static com.example.password_validator.MainActivity.LowerAndUpper;
 import static com.example.password_validator.MainActivity.atLeastOneSpecialChar;
+import static com.example.password_validator.MainActivity.atLeastEight;
+import static com.example.password_validator.MainActivity.notEqualPassword;
 
 
 
@@ -18,6 +20,18 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void not_Password() throws Exception {
+        String pwd = "HelloWorld";
+        boolean notEqualPassword = notEqualPassword(pwd);
+        assertTrue(notEqualPassword);
+    }
+    @Test
+    public void AtLeastEight() throws Exception{
+        String pwd = "HelloWorld";
+        boolean atLeastEight = atLeastEight(pwd);
+        assertTrue(atLeastEight);
     }
     @Test
     public void atLeast_OneDigit() throws Exception{
